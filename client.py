d83e412062d6c27437b85b2ccabf389ae60218da
Omri Krelman <83236913+Omrikre@users.noreply.github.com>
2022-12-03 17:55:32 +0200

    ex1 without

--- client.py
+++ client.py
@@ -1,40 +1,39 @@
 from datetime import datetime
 import requests
 
-url = "localhost:8989"
+if __name__ == '__main__':
+    # request 1 - GET
+    hour = datetime.now().hour
+    minute = datetime.now().minute
+    param = "hour=" + str(hour) + "&" + "minute=" + str(minute)
 
-# request 1 - GET
-hour = datetime.now().hour
-minute = datetime.now().minute
-param = "hour=" + str(hour) + "&" + "minute=" + str(minute)
+    print("1 - GET")
+    print("request: " + url + "/test_get_method?" + param)
+    responseGet = requests.get('http://localhost:8989/test_get_method', params={'hour': hour, 'minute': minute})
+    print("response: " + responseGet.text)
 
-print("1 - GET")
-print("request: " + url + "/test_get_method?" + param)
-responseGet = requests.get(url + "/test_get_method?" + param)
-print("response: " + responseGet.text)
+    # request 2 - POST
+    bodyPost = {
+        'hour': hour,
+        'minute': minute,
+        'requestId': responseGet.text}
 
-# request 2 - POST
-bodyPost = {
-    'hour': hour,
-    'minute': minute,
-    'requestId': responseGet.text}
+    print("2 - POST")
+    print("request: " + url + "/test_post_method")
+    print("data: " + str(bodyPost))
+    responsePost = requests.post('http://localhost:8989/test_post_method', data=bodyPost)
+    print("response: " + responsePost.text)
 
-print("2 - POST")
-print("request: " + url + "/test_post_method")
-print("data: " + str(bodyPost))
-responsePost = requests.post(url + "/test_post_method", data=bodyPost)
-print("response: " + responsePost.text)
+    # request 3 - PUT
+    messagePost = responsePost.json()['message']
+    hour = (hour + 21) % 24
+    minute = (minute + 13) % 60
+    bodyPut = {
+        "hour": hour,
+        "minute": minute
+    }
+    responsePut = requests.put('http://localhost:8989/test_put_method', params={"id": messagePost}, data=bodyPut)
+    messagePut = responsePut.json()['message']
 
-# request 3 - PUT
-messagePost = responsePost.json()['message']
-hour = (hour + 21) % 24
-minute = (minute + 13) % 60
-bodyPut = {
-    "hour": hour,
-    "minute": minute
-}
-responsePut = requests.put(url+"/test_put_method", params={"id": messagePost}, data=bodyPut)
-messagePut = responsePut.json()['message']
-
-# request 4 - DELETE
-requests.delete(url + "/test_delete_method", params={"id": messagePut})
+    # request 4 - DELETE
+    requests.delete('http://localhost:8989/test_delete_method', params={"id": messagePut})
